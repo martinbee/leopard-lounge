e0853cb798dfe0026da603e4bfb9d288f95800fb
Martin <martbee88@gmail.com>
2020-01-13 14:25:58 -0600

    feat: get working naive ui for tic tac toe

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Typography,
 } from '@material-ui/core';
 import styled from 'styled-components';
 
+
 const Text = styled(Typography)`
   text-align: center;
+`;
+
+const BoxWrapper = styled.div`
+  height: 42vh;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Box = styled.div`
+  border-right: ${({ border }) => border === 'horizontals' && '1px solid black'};
+  border-left: ${({ border }) => border === 'horizontals' && '1px solid black'};
+  border-top: ${({ border }) => border === 'verticals' && '1px solid black'};
+  border-bottom: ${({ border }) => border === 'verticals' && '1px solid black'};
+  border: ${({ border }) => border === 'all' && '1px solid black'};
+  text-align: center;
+  height: 14vh;
+  width: 33%;
 `;
 
 // 9 squares with appropriate borders
@@ -13,12 +30,92 @@ const Text = styled(Typography)`
 // once a square has been clicked it belongs to that player
 // that square can't be clicked again
 // when three in a row (horiz, diag, vertical) the player who owns those three wins (need to figure out algo)
+const initialClaimMap = {
+  0: {
+    player: '',
+    border: '',
+  },
+  1: {
+    player: '',
+    border: 'horizontals',
+  },
+  2: {
+    player: '',
+    border: '',
+  },
+  3: {
+    player: '',
+    border: 'verticals',
+  },
+  4: {
+    player: '',
+    border: 'all',
+  },
+  5: {
+    player: '',
+    border: 'verticals',
+  },
+  6: {
+    player: '',
+    border: '',
+  },
+  7: {
+    player: '',
+    border: 'horizontals',
+  },
+  8: {
+    player: '',
+    border: '',
+  },
+};
 
 const TicTacToe = () => {
-  return (
-    <div style={{ border: '1px solid grey', height: '42vh' }}>
+  const [currentPlayer, setCurrentPlayer] = useState('1');
+  const [claimMap, setClaimMap] = useState(initialClaimMap);
 
-    </div>
+  const toggleCurrentPlayer = () => {
+    if (currentPlayer === '1') {
+      setCurrentPlayer('2');
+    } else {
+      setCurrentPlayer('1');
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <Text>Turn: Player {currentPlayer}</Text>
+      </div>
+      <BoxWrapper>
+        {Object.entries(claimMap).map(([key, properties]) => {
+          const { player, border } = properties;
+
+          const claimBox = () => {
+            if (!player) {
+              const updatedClaimMap = {
+                ...claimMap,
+                [key]: {
+                  player: currentPlayer,
+                  border,
+                },
+              };
+              setClaimMap(updatedClaimMap);
+              toggleCurrentPlayer();
+            }
+          };
+
+          return (
+            <Box
+              key={key}
+              border={border}
+              onClick={claimBox}
+            >
+              {player}
+            </Box>
+          );
+        })}
+      </BoxWrapper>
+    </>
   );
 };
 
